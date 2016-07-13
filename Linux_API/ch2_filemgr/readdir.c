d737193b70e260ff69fdbe46d8c4dc13c6a42769
fa1c0n1 <t_macfalcon@163.com>
2016-07-13 16:33:41 +0800

    Linux_API:     add chmod2.c stat.c umask.c readdir.c seekdir.c umask_test.txt rdonly.txt

--- Linux_API/ch2_filemgr/readdir.c
+++ Linux_API/ch2_filemgr/readdir.c
@@ -8,16 +8,55 @@
 #include <dirent.h>
 #include <errno.h>
 
+#define MAX    1024
+
 int get_file_count(char *root)
 {
 	int total = 0;
+	DIR *dir;
+	struct dirent *ptr;
+	char path[MAX];
+
+	dir = opendir(root);
+	if (dir == NULL) {
+		perror("fail to opendir");
+		exit(1);
+	}
+
+	errno = 0;
+
+	while ((ptr = readdir(dir)) != NULL) {
+		if (strcmp(ptr->d_name, ".") == 0 || strcmp(ptr->d_name, "..") == 0) continue;
+		if (ptr->d_type == DT_DIR) {
+			sprintf(path, "%s/%s", root, ptr->d_name);
+			total += get_file_count(path);
+		} else if (ptr->d_type == DT_REG) {
+			total++;
+		}
+
+	}
+
+	if (errno != 0) {
+		perror("fail to readdir");
+		exit(1);
+	}
+
+	closedir(dir);
 
 	return total;
 }
 
 int main(int argc, char **argv)
 {
+	int total = 0;
 
+	if(argc != 2) {
+		printf("Wrong Usage!\n");
+		exit(1);
+	}
+
+	total = get_file_count(argv[1]);
+	printf("%s has %d files\n", argv[1], total);
 
 	return 0;
 }
