572d3fa017ce187e6dab35113aa3b9032490b0da
fa1c0n1 <t_macfalcon@163.com>
2019-02-12 07:06:08 +0800

    add SwingLearn

---
+++ SwingLearn/Demo8_BorderLayout/src/MainTest.java
@@ -0,0 +1,34 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+import java.awt.BorderLayout;
+
+public class MainTest {
+
+    public static void main(String[] args) {
+        JFrame jf = new JFrame("测试窗口");
+        jf.setSize(350, 250);
+        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        // 创建5个按钮
+        JPanel panel = new JPanel(new BorderLayout());
+        JButton btn01 = new JButton("Btn_North");
+        JButton btn02 = new JButton("Btn_South");
+        JButton btn03 = new JButton("Btn_Center");
+        JButton btn04 = new JButton("Btn_East");
+        JButton btn05 = new JButton("Btn_West");
+
+        // 把5个按钮添加到容器中的5个方位
+        panel.add(btn01, BorderLayout.EAST);
+        panel.add(btn02, BorderLayout.WEST);
+        panel.add(btn03, BorderLayout.CENTER);
+        panel.add(btn04, BorderLayout.SOUTH);
+        panel.add(btn05, BorderLayout.NORTH);
+
+        jf.setContentPane(panel);
+        jf.setLocationRelativeTo(null);
+        jf.setVisible(true);
+    }
+}
+
---
+++ SwingLearn/Demo9_SpringLayout/src/MainTest.java
@@ -0,0 +1,39 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+import javax.swing.WindowConstants;
+
+public class MainTest {
+
+    public static void main(String[] args) {
+        // 创建窗口
+        JFrame jf = new JFrame("测试窗口");
+        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jf.setSize(300, 200);
+        jf.setLocationRelativeTo(null);
+
+        // 创建内容面板, 使用弹性布局
+        SpringLayout layout = new SpringLayout();
+        JPanel panel = new JPanel(layout);
+        jf.setContentPane(panel);
+
+        // 创建组件
+        JLabel label = new JLabel("Test JLabel: ");
+        JButton btn = new JButton("Btn");
+        JTextField textField = new JTextField("Text Field");
+
+        // 添加组件到内容面板
+        panel.add(label);
+        panel.add(btn);
+        panel.add(textField);
+
+        /**
+         * 组件的约束设置(弹性布局设置的关键);
+         */
+
+        // 标签组件的约束:
+    }
+}
